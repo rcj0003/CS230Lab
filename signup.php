57770976c9363aaabb833048f4048e9156afc01e
Ryan Jones <30604957+rcj0003@users.noreply.github.com>
2020-10-11 02:56:41 -0400

    Correct character classes for regex on signup page

--- signup.php
+++ signup.php
@@ -14,18 +14,18 @@ require "includes/header.php"
 
                     <div class = "vertical-space-10">
                         <div class = "multi-input-line">
-                            <input class = "form-control" type = "text" pattern = "A-Za-z'- {1,32}" id = "first_name" placeholder = "First Name"  maxlength = "32" required autofocus>
-                            <input class = "form-control" type = "text" pattern = "A-Za-z'- {1,32}" id = "last_name" placeholder = "Last Name"  maxlength = "32" required autofocus>
+                            <input class = "form-control" type = "text" pattern = "[A-Za-z'- ]{1,32}" id = "first_name" placeholder = "First Name"  maxlength = "32" required autofocus>
+                            <input class = "form-control" type = "text" pattern = "[A-Za-z'- ]{1,32}" id = "last_name" placeholder = "Last Name"  maxlength = "32" required autofocus>
                         </div>
                     </div>
 
                     <input class = "form-control" type = "email" id = "email" placeholder = "Email"  maxlength = "64" required autofocus>
-                    <input class = "form-control" type = "text" pattern = "0-9A-Za-z!_{4,20}" id = "username" placeholder = "Username"  maxlength = "20" required autofocus>
+                    <input class = "form-control" type = "text" pattern = "[0-9A-Za-z!_]{4,20}" id = "username" placeholder = "Username"  maxlength = "20" required autofocus>
 
                     <div class = "vertical-space-10">
                         <div class = "multi-input-line">
-                            <input class = "form-control" type = "password" pattern = "0-9A-Za-z!@#$_{8,64}" id = "password" placeholder = "Password (8 chars min.)" maxlength = "64" required>
-                            <input class = "form-control" type = "confirm_password" pattern = "0-9A-Za-z!@#$_{8,64}" id = "password" placeholder = "Confirm Password" maxlength = "64" required>
+                            <input class = "form-control" type = "password" pattern = "[0-9A-Za-z!@#$_]{8,64}" id = "password" placeholder = "Password (8 chars min.)" maxlength = "64" required>
+                            <input class = "form-control" type = "confirm_password" pattern = "[0-9A-Za-z!@#$_]{8,64}" id = "password" placeholder = "Confirm Password" maxlength = "64" required>
                         </div>
                     </div>
 
