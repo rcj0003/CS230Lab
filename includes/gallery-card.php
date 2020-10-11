08045664f3ef9722f2c8dcbec2b019b31c024297
Ryan Jones <30604957+rcj0003@users.noreply.github.com>
2020-10-11 17:08:37 -0400

    Create template for gallery cards

---
+++ includes/gallery-card.php
@@ -0,0 +1,7 @@
+<div class = "card">
+    <a href = "#">
+        <img src = "<?php echo($row['picpath']); ?>">
+        <h3><?php echo($row['title']); ?><h3>
+        <p><?php echo($row['description']); ?></p>
+    </a>
+</div>
