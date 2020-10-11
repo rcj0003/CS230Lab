3b2df1b016841974f44b358c5a238eef2947fde0
Ryan Jones <30604957+rcj0003@users.noreply.github.com>
2020-10-11 17:08:47 -0400

    Create gallery page

--- gallery.php
+++ gallery.php
@@ -3,7 +3,17 @@ require 'includes/header.php';
 ?>
 
 <main>
-<link rel="stylesheet" href="css/gallery.css">
-    
+    <link rel="stylesheet" href="css/gallery.css">
+    <h1>Product Gallery</h1>
+    <div class = "gallery-container">
+        <?php
+            require 'includes/gallery-fetcher.php';
 
+            $gallery = fetchGallery();
+
+            foreach ($gallery as $row) {
+                include 'includes/gallery-card.php';
+            }
+        ?>
+    </div>
 </main>
