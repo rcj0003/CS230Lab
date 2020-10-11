7135fae7e6ca506eb9c0dbc992a319eef52bf585
Ryan Jones <30604957+rcj0003@users.noreply.github.com>
2020-10-11 03:44:20 -0400

    Rename login.html to login.php

--- includes/header.php
+++ includes/header.php
@@ -33,7 +33,7 @@
         <div class = "collapse navbar-collapse" id = "navbarSupportedContent">
             <ul class = "navbar-nav mr-auto">
                 <li class = "nav-item">
-                    <a class = "nav-link" href = "login.html">Login</a>
+                    <a class = "nav-link" href = "login.php">Login</a>
                 </li>
 
                 <li class = "nav-item">
