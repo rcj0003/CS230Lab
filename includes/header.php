1e0fef9b2a9e8d50873d1b58587a5ee5e2574bae
Ryan Jones <30604957+rcj0003@users.noreply.github.com>
2020-10-11 04:03:27 -0400

    Update header to link to about.php

--- includes/header.php
+++ includes/header.php
@@ -33,12 +33,12 @@
         <div class = "collapse navbar-collapse" id = "navbarSupportedContent">
             <ul class = "navbar-nav mr-auto">
                 <li class = "nav-item">
-                    <a class = "nav-link" href = "login.php">Login</a>
+                    <a class = "nav-link" href = "../login.php">Login</a>
                 </li>
 
                 <li class = "nav-item">
                     <li class = "nav-item active">
-                        <a class = "nav-link" href = "#">About <span class = "sr-only">(current)</span></a>
+                        <a class = "nav-link" href = "../about.php">About <span class = "sr-only">(current)</span></a>
                     </li>
                 </li>
 
