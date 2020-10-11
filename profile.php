746afba00092f08163d15f6d6052cb947e3721bc
Ryan Jones <30604957+rcj0003@users.noreply.github.com>
2020-10-11 05:17:15 -0400

    Create profile page after being logged in

--- profile.php
+++ profile.php
@@ -3,6 +3,12 @@ require 'includes/header.php'
 ?>
 
 <main>
-        
-      
+    <?php
+        if (!isset($_SESSION['uid'])) {
+            header("Location: ../login.php");
+            exit();
+        }
+
+        include 'html/profile.html'
+    ?>
 </main>
