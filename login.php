118a5c43358e3819a206f01116b487ef8820d7b0
Ryan Jones <30604957+rcj0003@users.noreply.github.com>
2020-10-11 04:16:12 -0400

    Add names to login.php inputs / buttons

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
     <body class = "no-scroll">
         <div class = "bg-cover">
             <div class = "row">
-                <div id = "loginCarousel" class = "carousel slide col-md-8 offset-md-2" data-ride = "carousel" style = "margin-top: 50px; margin-bottom: 50px; border-radius: 5%;">
+                <div id =  "loginCarousel" class = "carousel slide col-md-8 offset-md-2" data-ride = "carousel" style = "margin-top: 50px; margin-bottom: 50px; border-radius: 5%;">
                     <div class = "carousel-inner">
                         <div class = "carousel-item active">
                             <img class = "d-block mx-auto img-limit" src = "../images/bg1.jpg" alt = "First slide">
@@ -30,12 +30,12 @@
                             <h1>Sign in</h1>
                         </div>
                         
-                        <input class = "form-control" type = "email" id = "email" placeholder = "Email"  maxlength = "64" required autofocus>
-                        <input class = "form-control" type = "password" pattern = "0-9A-Za-z!@#$_{8,64}" id = "password" placeholder = "Password" maxlength = "64" required>
+                        <input class = "form-control" type = "email" name =  "email" placeholder = "Email"  maxlength = "64" required autofocus>
+                        <input class = "form-control" type = "password" pattern = "0-9A-Za-z!@#$_{8,64}" name =  "password" placeholder = "Password" maxlength = "64" required>
                         
                         <div class = "checkbox vertical-space-10">
                             <label for = "remember">
-                                <input type = "checkbox" id = "remember"> Stay signed in
+                                <input type = "checkbox" name = "remember"> Stay signed in
                             </label>
                         </div>
 
@@ -46,7 +46,7 @@
                             </p>
                         </div>
 
-                        <button class = "btn btn-info">Sign in</button>
+                        <button class = "btn btn-info" name = "login-submit">Sign in</button>
                     </form>
                 </div>
             </div>
