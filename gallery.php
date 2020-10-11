8a4f99792036f0f844b0300e1d226760ab1b4a8f
Ryan Jones <30604957+rcj0003@users.noreply.github.com>
2020-10-11 17:45:56 -0400

    Complete gallery page

--- gallery.php
+++ gallery.php
@@ -4,16 +4,20 @@ require 'includes/header.php';
 
 <main>
     <link rel="stylesheet" href="css/gallery.css">
-    <h1>Product Gallery</h1>
-    <div class = "gallery-container">
-        <?php
-            require 'includes/gallery-fetcher.php';
+    <div class = "bg-cover">
+        <div class = "center modal-lg">
+            <h1>Product Gallery</h1>
+            <div class = "card-container">
+                <?php
+                    require 'includes/gallery-fetcher.php';
 
-            $gallery = fetchGallery();
+                    $gallery = fetchGallery();
 
-            foreach ($gallery as $row) {
-                include 'includes/gallery-card.php';
-            }
-        ?>
+                    foreach ($gallery as $row) {
+                        include 'includes/gallery-card.php';
+                    }
+                ?>
+            </div>
+        </div>
     </div>
 </main>
--- includes/gallery-card.php
+++ includes/gallery-card.php
@@ -1,4 +1,4 @@
-<div class = "card">
+<div class = "col-md-3">
     <a href = "#">
         <img src = "<?php echo($row['picpath']); ?>">
         <h3><?php echo($row['title']); ?><h3>
