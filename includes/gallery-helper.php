8026d5abd14fb637a4ad3cc14386208b17a41042
Ryan Jones <30604957+rcj0003@users.noreply.github.com>
2020-10-11 16:43:42 -0400

    Finish gallery-helper.php, ignore files in products/

--- includes/gallery-helper.php
+++ includes/gallery-helper.php
@@ -1,6 +1,63 @@
 <?php 
-
 require 'dbhandler.php';
-session_start();
-?>
 
+define('KB', 1024);
+define('MB', 1048576);
+
+if (isset($_POST['gallery-submit'])) {
+    $file = $_FILES['gallery-image'];
+    $fileName = $file['name'];
+    $fileTmpName = $file['tmp_name'];
+    $fileError = $file['error'];
+    $fileSize = $file['size'];
+
+    $title = $_POST['title'];
+    $dscpt = $_POST['desc'];
+
+    $ext = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
+
+    $allowed = array('jpg', 'jpeg', 'png', 'svg');
+
+    if ($fileError !== 0) {
+        header("Location: ../admin.php?error=UploadError");
+        exit();
+    }
+
+    if (!in_array($ext, $allowed)) {
+        header("Location: ../admin.php?error=InvalidType");
+        exit();
+    }
+
+    if ($fileSize > 4 * MB) {
+        header("Location: ../admin.php?error=FileSizeExceeded");
+        exit();
+    } else {
+        $newName = uniqid('',true).".".$ext;
+        $destination = "../products/".$newName;
+
+        $sql = "INSERT INTO products (picpath, title, description) VALUES (?,?,?)";
+        $statement = $conn->stmt_init();
+
+        if ($statement->prepare($sql)) {
+            $statement->bind_param("sss", $destination, $title, $dscpt);
+            $statement->execute();
+            $statement->store_result();
+    
+            move_uploaded_file($fileTmpName, $destination);
+    
+            $statement->close();
+            $conn->close();
+
+            header("Location: ../admin.php?success=UploadedSuccess");
+            exit();
+        } else {
+            header("Location: ../admin.php?error=SQLInjection");
+            exit();
+        }
+    }
+} else {
+    header("Location: ../admin.php");
+    exit();
+}
+
+?>
