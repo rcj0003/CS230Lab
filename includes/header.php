55b5c182512584abea68b2530d6e8efcfc83ea62
Ryan Jones <30604957+rcj0003@users.noreply.github.com>
2020-10-11 03:35:00 -0400

    Update header

--- includes/header.php
+++ includes/header.php
@@ -23,36 +23,42 @@
 
     </head>
     <header>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">Fanatic</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <nav class = "navbar navbar-dark bg-dark">
+        <a class = "navbar-brand" href = "#">OzDev</a>
+        
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="login.html">Login</a>
+        <div class = "collapse navbar-collapse" id = "navbarSupportedContent">
+            <ul class = "navbar-nav mr-auto">
+                <li class = "nav-item">
+                    <a class = "nav-link" href = "login.html">Login</a>
                 </li>
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li> 
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Reviews
+
+                <li class = "nav-item">
+                    <li class = "nav-item active">
+                        <a class = "nav-link" href = "#">About <span class = "sr-only">(current)</span></a>
+                    </li>
+                </li>
+
+                <li class = "nav-item dropdown">
+                    <a class = "nav-link dropdown-toggle" href = "#" id = "navbarDropdown" role = "button" data-toggle = "dropdown" aria-haspopup = "true" aria-expanded = "false">
+                        Me
                     </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Replace Me</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Replace Me 2</a>
+
+                    <div class = "dropdown-menu" aria-labelledby = "navbarDropdown">
+                        <a class = "dropdown-item" href = "#">Settings</a>
+                        <a class = "dropdown-item" href = "#">My Products</a>
+                        <div class = "dropdown-divider"></div>
+                        <a class = "dropdown-item" href = "#">Logout</a>
                     </div>
                 </li>
             </ul>
-            <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            
+            <form class = "form-inline my-2 my-lg-0">
+                <input class = "form-control mr-sm-2" type = "search" placeholder = "Search" aria-label = "Search">
+                <button class = "btn btn-info my-2 my-sm-0" type = "submit">Search for product</button>
             </form>
         </div>
     </nav>
