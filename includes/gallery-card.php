8a2b413f09a3d7fbc654447c012200dfa007ed59
Ryan Jones <30604957+rcj0003@users.noreply.github.com>
2020-10-25 20:49:49 -0400

    Update gallery card to link to review page for product

--- includes/gallery-card.php
+++ includes/gallery-card.php
@@ -1,5 +1,5 @@
 <div class = "col-md-3">
-    <a href = "#">
+    <a href = "review.php?id=<?php echo($row['pid']); ?>">
         <img src = "<?php echo($row['picpath']); ?>">
         <h3><?php echo($row['title']); ?><h3>
         <p><?php echo($row['description']); ?></p>
