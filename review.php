beec473c3f2f3a9ede6b3c3440495f4c9033e0f1
Ryan Jones <30604957+rcj0003@users.noreply.github.com>
2020-10-25 21:45:04 -0400

    Update review page

--- review.php
+++ review.php
@@ -6,11 +6,35 @@ require 'includes/review-helper.php';
 
 <main>
     <span id="testAvg"></span>
-    
+    <div class = "container" align = "center" style = "max-width: 800px">
+        <div class = "my-auto">
+            <form id = "review-form" action = "includes/review-helper.php" method = "post">
+                <div class = "container">
+                    <i class = "fa fa-star fa-2x star-rev" data-index = "1"></i>
+                    <i class = "fa fa-star fa-2x star-rev" data-index = "2"></i>
+                    <i class = "fa fa-star fa-2x star-rev" data-index = "3"></i>
+                    <i class = "fa fa-star fa-2x star-rev" data-index = "4"></i>
+                    <i class = "fa fa-star fa-2x star-rev" data-index = "5"></i>
+                </div>
+                <div class = "form-group" style = "margin-top: 16px;">
+                    <label class = "title-label" form = "review-title" style = "width: 100%;"></label>
+                    <input type = "text" name = "review-title" id = "review-title" style = "width: 100%; margin-bottom: 16px;" placeholder = "Your title"></input>
+                    <textarea style = "resize: none; overflow-wrap: anywhere;" class = "form-control" id = "review-text" name = "review" placeholder = "Your review here..." cols = "50" rows = "3"></textarea>
+                    <input type = "hidden" name = "rating" id = "rating"></input>
+                    <input type = "hidden" name = "item_id" value = "<?php echo($_GET['id']); ?>"></input>
+                </div>
+                <div class = "form-group">
+                    <button name = "review-submit" class = "btn btn-info">Post Review</button>
+                </div>
+            </form>
+        </div>
+    </div>
     <span id="review_list"></span>
 </main>
 <script type="text/javascript">
 
+var rateIndex = -1;
+var id = <?php echo($_GET['id']); ?>;
 
 $(document).ready(function() {
     reset_star();
@@ -46,8 +70,26 @@ $(document).ready(function() {
         $('.star-rev').css('color', 'grey');
     }
 
+    function setStars(max) {
+        for (var i = 0; i < max; i++) {
+            $('.star-rev:eq('+i+')').css('color', 'grey');
+        }
+        document.getElementById('rating').value = parseInt(localStorage.getItem('rating'));
+    }
 
-    //Used to interchangeably send GET requests for review display data. 
-    
+    //Used to interchangeably send GET requests for review display data.
+    function xhr_getter(prefix, element) {
+        var xhttp = new XMLHttpRequest();
+        
+        xhttp.onreadystatechange = function() {
+            if (this.readyState = 4 && status == 200) {
+                document.getElementById(element).innerHTML = this.responseText;
+            }
+        };
+
+        url = prefix + id;
+        xhttp.open("GET", url, true);
+        xhttp.send();
+    }
 });
 </script>
