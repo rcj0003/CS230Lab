718464db67916ef2d92cb759b51a1eee99dbfd02
Ryan Jones <30604957+rcj0003@users.noreply.github.com>
2020-11-19 15:31:50 -0500

    Add generalized upload controller

---
+++ includes/controllers/upload-controller.php
@@ -0,0 +1,45 @@
+<?php
+
+define('KB', 1024);
+define('MB', 1048576);
+
+class FileException extends Exception {
+    function __construct($message) {
+        parent::__construct($message);
+    }
+}
+
+class UploadController {
+    function verifyAndUploadImageFile($fileElement) {
+        $file = $_FILES[$fileElement];
+        $fileName = $file['name'];
+        $fileTmpName = $file['tmp_name'];
+        $fileError = $file['error'];
+        $fileSize = $file['size'];
+    
+        $title = $_POST['title'];
+        $dscpt = $_POST['desc'];
+    
+        $ext = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
+    
+        $allowed = array('jpg', 'jpeg', 'png', 'svg');
+    
+        if ($fileError !== 0) {
+            throw new FileException("Unknown error occured while uploading file.");
+        }
+    
+        if (!in_array($ext, $allowed)) {
+            throw new FileException("Extension not allowed.");
+        }
+    
+        if ($fileSize > 4 * MB) {
+            throw new FileException("File exceeds maximum size.");
+        }
+
+        move_uploaded_file($fileTmpName, $destination);
+    }
+}
+
+$uploadController = new UploadController;
+
+?>
