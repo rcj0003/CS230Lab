6f72067a0c2f5b4052657905f0404a56bd2cb437
Ryan Jones <30604957+rcj0003@users.noreply.github.com>
2020-11-19 16:08:19 -0500

    Add review / review data to gallery controller

--- includes/controllers/gallery-controller.php
+++ includes/controllers/gallery-controller.php
@@ -29,9 +29,26 @@ class GalleryController {
         executePrepared("INSERT INTO projects (".self::POSTER_ID_FIELD.", ".self::STATUS_FIELD.", ".self::TITLE_FIELD.", ".self::DESCRIPTION_FIELD.", ".self::ICON_FIELD.", ".self::DOWNLOAD_LINK_FIELD.") VALUES (?, ?, ?, ?, ?, ?)", "ssssss", $loginController->getUserId(), 1, $title, $description, $destination, $downloadLink);
     }
 
+    function getReviewData($galleryId, $quantity) {
+        require 'sql-helper.php';
+        return executeQuery("SELECT * FROM reviews WHERE ".self::PROJECT_ID_FIELD."=? LIMIT ?", "ii", $galleryId, $quantity);
+    }
+
+    function createReview($galleryId, $rating, $title, $description) {
+        require 'login-controller.php';
+        require 'exceptions.php';
+
+        if (!$loginController->isLoggedIn()) {
+            throw new UserNotLoggedInException();
+        }
+
+        require 'sql-helper.php';
+        executePrepared("INSERT INTO reviews (".self::POSTER_ID_FIELD.", ".self::TITLE_FIELD.", ".self::DESCRIPTION_FIELD.", ".self::RATING_FIELD.") VALUES (?, ?, ?, ?)", "ssssss", $loginController->getUserId(), $title, $description, $rating);
+    }
+
     function fetchGalleryEntries($pageNumber) {
         require 'pagination.php';
-        $paginator = new Paginator(25, "SELECT * FROM projects", "");
+        $paginator = new Paginator(25, "SELECT * FROM projects WHERE ".self::STATUS_FIELD."=1", "");
         return $paginator->getPageNumber($pageNumber);
     }
 }
